18a80f6da1d657275084d28b5069c1e436848f48
iness02 <inesahakobyan18@gmail.com>
2024-08-21 08:52:23 +0400

    trainer workload implementation

--- trainer-workload/src/main/java/com/example/trainer_workload/TrainerWorkloadApplication.java
+++ trainer-workload/src/main/java/com/example/trainer_workload/TrainerWorkloadApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class TrainerWorkloadApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TrainerWorkloadApplication.class, args);
 	}
 
